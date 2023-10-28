ca60a7a59f7c1d34458cd41f6dd7a28318940192
UrMBCMRabbont <philipt710mh@gmail.com>
2023-10-28 23:02:03 +0800

    not work

--- include/teb_local_planner/sample_cv_bridge_node.hpp
+++ include/teb_local_planner/sample_cv_bridge_node.hpp
@@ -63,31 +63,34 @@ public:
   {
     cv::Mat local_map = cv::Mat::zeros(global_map.size(), CV_8UC1);;
     for (size_t i = 0; i < ptcloud.points.size(); ++i) {
-        cv::Point pt1(ptcloud.points[i].x-1, ptcloud.points[i].y-1);
-        // and its bottom right corner.
-        cv::Point pt2(ptcloud.points[i].x+1, ptcloud.points[i].x+1);
-        // These two calls...
-        cv::rectangle(local_map, pt1, pt2, cv::Scalar(255, 255, 255));
+        // cv::Point pt1(ptcloud.points[i].x-1, ptcloud.points[i].y-1);
+        // // and its bottom right corner.
+        // cv::Point pt2(ptcloud.points[i].x+1, ptcloud.points[i].x+1);
+        // // These two calls...
+        // cv::rectangle(local_map, pt1, pt2, cv::Scalar(255, 255, 255));
+        //local_map.at<int>(int(ptcloud.points[i].y*10.0), int(ptcloud.points[i].x/0.1)*10.0) = 255;//Changing the value of pixel//
     }
-    vector<vector<cv::Point>> contours;
-    vector<cv::Vec4i> hierarchy,hierarchy2;
-    findContours(local_map,contours,hierarchy,cv::RETR_TREE,cv::CHAIN_APPROX_TC89_L1 ,cv::Point()); 
-        // Simplify the shape of each contour
+    // cv::imshow("idk",local_map);
+    // cv::waitKey(1);
+    // vector<vector<cv::Point>> contours;
+    // vector<cv::Vec4i> hierarchy,hierarchy2;
+    // findContours(local_map,contours,hierarchy,cv::RETR_TREE,cv::CHAIN_APPROX_TC89_L1 ,cv::Point()); 
+    //     // Simplify the shape of each contour
 
-    std::vector<std::vector<cv::Point>> simplifiedContours(contours.size());
-    for (size_t i = 0; i < contours.size(); ++i) {
-        cv::approxPolyDP(contours[i], simplifiedContours[i],1.0, true);  // Adjust the epsilon value as needed
-    }
+    // std::vector<std::vector<cv::Point>> simplifiedContours(contours.size());
+    // for (size_t i = 0; i < contours.size(); ++i) {
+    //     cv::approxPolyDP(contours[i], simplifiedContours[i],1.0, true);  // Adjust the epsilon value as needed
+    // }
 
-    // Draw the simplified contours on a black image
-    cv::Mat resultImage = cv::Mat::zeros(global_map.size(), CV_8UC1);
-    cv::Mat resultImage2 = cv::Mat::zeros(global_map.size(), CV_8UC1);
-    ROS_INFO("start cv2");
-    cv::drawContours(resultImage, simplifiedContours, -1, cv::Scalar(255, 255, 255), FILLED);
-    findContours(resultImage,obj_con,hierarchy,cv::RETR_TREE,cv::CHAIN_APPROX_SIMPLE,cv::Point()); 
-    ROS_INFO("start cv3");
-    cv::drawContours(resultImage2, obj_con, -1, cv::Scalar(255, 255, 255), 1);
-    ROS_INFO("start cv4");
+    // // Draw the simplified contours on a black image
+    // cv::Mat resultImage = cv::Mat::zeros(global_map.size(), CV_8UC1);
+    // cv::Mat resultImage2 = cv::Mat::zeros(global_map.size(), CV_8UC1);
+    // ROS_INFO("start cv2");
+    // cv::drawContours(resultImage, simplifiedContours, -1, cv::Scalar(255, 255, 255), FILLED);
+    // findContours(resultImage,obj_con,hierarchy,cv::RETR_TREE,cv::CHAIN_APPROX_SIMPLE,cv::Point()); 
+    // ROS_INFO("start cv3");
+    // cv::drawContours(resultImage2, obj_con, -1, cv::Scalar(255, 255, 255), 1);
+    // ROS_INFO("start cv4");
       
   }
   vector<vector<cv::Point>> get_obj_pt(){
